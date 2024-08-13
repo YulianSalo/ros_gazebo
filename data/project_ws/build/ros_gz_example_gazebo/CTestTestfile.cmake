05f25202088f993dc8106c33069fd0e4ca4b428d
yuliansalo <julian_salo@yahoo.com>
2024-08-13 10:28:55 +0300

    all added

---
+++ data/project_ws/build/ros_gz_example_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /root/data/project_ws/src/ros_gz_example_gazebo
+# Build directory: /root/data/project_ws/build/ros_gz_example_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
