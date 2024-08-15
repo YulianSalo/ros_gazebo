1a474ed49f10b045f2af53789fd03cfca722d2e1
saloy2 <saloy2@medtronic.com>
2024-08-15 18:08:28 +0300

    upd

--- data/project_ws/build/hardware_link2/CMakeFiles/hardware_link2_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/hardware_link2_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/hardware_link2_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
