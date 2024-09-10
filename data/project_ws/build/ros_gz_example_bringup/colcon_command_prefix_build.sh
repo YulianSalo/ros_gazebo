874a03801ff7b4cd4c145acd3f21e6d40cff236f
yuliansalo <julian_salo@yahoo.com>
2024-09-10 11:28:16 +0300

    upd

--- data/project_ws/build/ros_gz_example_bringup/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/root/data/project_ws/install/ros_gz_example_description/share/ros_gz_example_description/package.sh"
-. "/root/data/project_ws/install/ros_gz_example_gazebo/share/ros_gz_example_gazebo/package.sh"
